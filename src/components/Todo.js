210d93c24aeb96e1d19f79e8c4936ed50c6a930b
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-06-16 03:24:50 +0530

    commit

---
+++ src/components/Todo.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import './Todo.css';
+
+
+function Task({ task, index, completeTask, removeTask }) {
+    return (
+        <>
+         
+        <div
+       
+            className="task"
+        style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+            <button style={{ background: "rgb(128, 0, 32)",borderRadius:50,height:27,width:27}} onClick={() => removeTask(index)}>X</button>
+            <button onClick={() => completeTask(index)}>Completed</button>
+        </div>
+          </> 
+    );
+}
+
+function CreateTask({ addTask }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTask(value);
+        setValue("");
+       
+    }
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add a new task"
+                onChange={e => setValue(e.target.value)}    
+            />
+           
+             
+           
+           <button type='submit' className="btn">Add Task</button>
+        </form>
+        </>
+    );
+}
+
+function Todo() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState([
+        {
+            title: "Exercise",
+            completed: true
+        },
+        {
+            title: "Buy groceries",
+            completed: true
+        },
+        {
+            title: "Book tickets",
+            completed: false
+        }
+      
+    ]);
+
+    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) }, [tasks]);
+
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+       
+
+    };
+
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+        
+    };
+
+    return (
+        <div className="todo-container">
+            <h1 className='heading'>To-Do List</h1>
+            <div className="header">Task in Progress - [{tasksRemaining}]</div>
+            <div className="tasks">
+              
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    // date ={date}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index} 
+                    />
+                  
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+                
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
